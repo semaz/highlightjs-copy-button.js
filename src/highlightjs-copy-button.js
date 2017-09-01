b10e8e8a520ae7c84b7b22be6783b1b70b8a9a0e
creel <serjcreel@gmail.com>
2017-09-01 23:39:18 +0300

    add css semicolon

--- src/highlightjs-copy-button.js
+++ src/highlightjs-copy-button.js
@@ -72,7 +72,7 @@
                 'padding: 5px 10px;',
                 'margin: 5px;',
                 'border-radius: 5px;',
-                'border: 1px solid black',
+                'border: 1px solid black;',
                 'cursor: pointer;',
             '}'
         ].join('') ).format(BLOCK_NAME);
